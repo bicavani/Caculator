a3705a83332938285b783fd30393949f90a88573
Namlongcomputer <hoandm91@gmail.com>
2019-03-27 13:53:11 +0700

    version1

---
+++ Caculator.js
@@ -0,0 +1,20 @@
+function addition() {
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    alert(a+b);
+}
+function subtraction() {
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    alert(a-b);
+}
+function multiplication() {
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    alert(a*b);
+}
+function division() {
+    let a = parseInt(document.getElementById("number1").value);
+    let b = parseInt(document.getElementById("number2").value);
+    alert(a/b);
+}
